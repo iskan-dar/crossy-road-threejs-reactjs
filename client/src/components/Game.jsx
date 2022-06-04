0d1c826aba951e7f9e59f202fff06fe0229454a5
“AntonAtnagulov” <bebebe59@gmail.com>
2022-06-04 17:52:32 +0300

    Style Game

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable default-case */
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import style from './style.module.css';
 import * as THREE from 'three';
 import Chicken from '../generators/Chicken';
 import Grass from '../generators/Ground/Grass';
@@ -9,21 +10,32 @@ import Water from '../generators/Ground/Water';
 import Car from '../generators/Items/Car';
 import Truck from '../generators/Items/Truck';
 import generateLanes from '../generators/generateLanes';
-// import animate from '../generators/animate';
 import Lane from '../generators/Lane';
+import Restart from './Restart/Restart';
 
 export default function Game() {
     const mountRef = useRef(null);
+    const [isDead, setIsDead] = useState(false);
+    const [restart, setRestart] = useState(false);
+    let localIsDead = false;
+    let lanes;
+    const scene = new THREE.Scene();
 
     useEffect(() => {
-        const scene = new THREE.Scene();
         scene.background = new THREE.Color('#141517');
 
         const distance = 500;
         let height;
-        let cameraSpeed = 2
-        let positionY
-        let stepTime = 200
+        let cameraSpeed = 1.5;
+        let positionY;
+        let stepTime = 200;
+        let currentLane;
+        let currentColumn;
+        let previousTimestamp;
+        let startMoving;
+        let moves;
+        let stepStartTimestamp;
+        let chickenSize = 15
 
         const camera = new THREE.OrthographicCamera(
             window.innerWidth / -2,
@@ -47,6 +59,14 @@ export default function Game() {
         );
         dirLight.castShadow = true;
         scene.add(dirLight);
+
+        dirLight.shadow.mapSize.width = 1024;
+        dirLight.shadow.mapSize.height = 1024;
+        var d = 300;
+        dirLight.shadow.camera.left = -d;
+        dirLight.shadow.camera.right = d;
+        dirLight.shadow.camera.top = d;
+        dirLight.shadow.camera.bottom = -d;
 
         const backLight = new THREE.DirectionalLight(0x000000, 0.4);
         backLight.position.set(200, 200, 50);
@@ -73,21 +93,9 @@ export default function Game() {
         const vechicleColors = [0xa52523, 0xbdb638, 0x78b14b, 0x1a5b9c];
         const chicken = new Chicken(zoom)
         scene.add(chicken);
-        // =============================================================================================
-        let lanes;
-        let currentLane;
-        let currentColumn;
-
-        let previousTimestamp;
-        let startMoving;
-        let moves;
-        let stepStartTimestamp;
-
-        let chickenSize = 15
 
         const initaliseValues = () => {
             lanes = generateLanes(zoom,boardWidth, positionWidth, scene, vechicleColors, height);
-            // console.log('lanes}}}}}', lanes)
             currentLane = 0;
             currentColumn = Math.floor(columns / 2);
 
@@ -95,7 +103,6 @@ export default function Game() {
 
             startMoving = false;
             moves = [];
-            // stepStartTimestamp;
 
             chicken.position.x = 0;
             chicken.position.y = 0;
@@ -118,17 +125,6 @@ export default function Game() {
             lanes.push(lane);
           }
 
-
-          
-        // Test scene add ==============================================================================
-        // scene.add(new Road(zoom, boardWidth, positionWidth), Chicken(zoom));
-
-        // scene.add(
-        //     new Water(zoom, boardWidth, positionWidth),
-        //     Chicken(zoom),
-        //     new Truck(zoom, vechicleColors)
-        // );
-
           function move(direction) {
             const finalPositions = moves.reduce((position,move) => {
               if(move === 'forward') return {lane: position.lane + 1, column: position.column};
@@ -136,8 +132,6 @@ export default function Game() {
               if(move === 'left') return {lane: position.lane, column: position.column - 1};
               if(move === 'right') return {lane: position.lane, column: position.column + 1};
             }, {lane: currentLane, column: currentColumn})
-
-            // console.log(finalPositions)
 
             if (direction === 'forward') {
               if(lanes[finalPositions.lane+1].type === 'forest' && lanes[finalPositions.lane+1].occupiedPositions.has(finalPositions.column)) return;
@@ -163,25 +157,25 @@ export default function Game() {
           }
 
           window.addEventListener("keydown", event => {
-            if (event.keyCode == '38') {
+            if (event.keyCode == '38' && localIsDead === false) {
               // up arrow
               if (chicken.scale.z > 0.8) {
                   chicken.scale.z -= 0.2
               }
             }
-            else if (event.keyCode == '40') {
+            else if (event.keyCode == '40' && localIsDead === false) {
               // down arrow
               if (chicken.scale.z > 0.8) {
                 chicken.scale.z -= 0.2
             }
             }
-            else if (event.keyCode == '37') {
+            else if (event.keyCode == '37' && localIsDead === false) {
               // left arrow
               if (chicken.scale.z > 0.8) {
                 chicken.scale.z -= 0.2
             }
             }
-            else if (event.keyCode == '39') {
+            else if (event.keyCode == '39' && localIsDead === false) {
               // right arrow
               if (chicken.scale.z > 0.8) {
                 chicken.scale.z -= 0.2
@@ -190,35 +184,24 @@ export default function Game() {
           });
 
           window.addEventListener("keyup", event => {
-            if (event.keyCode == '38') {
-              console.log('currentColumn}}}}}}', currentColumn)
-              console.log('chickenPosition}}}}}', chicken.position.x)
+            if (event.keyCode == '38' && localIsDead === false) {
               chicken.scale.z = 1
               move('forward')
-            //   cameraSpeed += 0.01
+              cameraSpeed += 0.01;
             }
-            else if (event.keyCode == '40') {
+            else if (event.keyCode == '40' && localIsDead === false) {
               // down arrow
               chicken.scale.z = 1
-              console.log('backward');
-              console.log('currentColumn}}}}}}', currentColumn)
-              console.log('chickenPosition}}}}}', chicken.position.x)
               move('backward')
             }
-            else if (event.keyCode == '37') {
+            else if (event.keyCode == '37' && localIsDead === false) {
               // left arrow
               chicken.scale.z = 1
-              console.log('left');
-              console.log('currentColumn}}}}}}', currentColumn)
-              console.log('chickenPosition}}}}}', chicken.position.x)
               move('left')
             }
-            else if (event.keyCode == '39') {
+            else if (event.keyCode == '39' && localIsDead === false) {
               // right arrow
               chicken.scale.z = 1
-              console.log('right');
-              console.log('currentColumn}}}}}}', currentColumn)
-              console.log('chickenPosition}}}}}', chicken.position.x)
               move('right')
             }
           });
@@ -233,7 +216,8 @@ export default function Game() {
         renderer.setSize(window.innerWidth, window.innerHeight);
         mountRef.current.appendChild(renderer.domElement);
 
-        // requestAnimationFrame( animate );
+        dirLight.target = chicken;
+
         function animate(timestamp) {
             requestAnimationFrame( animate );
 
@@ -270,7 +254,6 @@ export default function Game() {
 
             });
 
-
             if(startMoving) {
               stepStartTimestamp = timestamp;
               startMoving = false;
@@ -283,7 +266,6 @@ export default function Game() {
               switch(moves[0]) {
                 case 'forward': {
                   const positionY = currentLane*positionWidth*zoom + moveDeltaDistance;
-                //   console.log(chicken)
                 if (chicken.position.y - 600 > camera.position.y) {
                     camera.position.y = initialCameraPositionY + positionY;
                 }
@@ -296,7 +278,6 @@ export default function Game() {
                 }
                 case 'backward': {
                   positionY = currentLane*positionWidth*zoom - moveDeltaDistance
-                //   camera.position.y = initialCameraPositionY + positionY;
                   dirLight.position.y = initialDirLightPositionY + positionY;
                   chicken.position.y = positionY;
 
@@ -359,57 +340,56 @@ export default function Game() {
               lanes[currentLane].vechicles.forEach(vechicle => {
                 const carMinX = vechicle.position.x - vechicleLength*zoom/2;
                 const carMaxX = vechicle.position.x + vechicleLength*zoom/2;
-                // if(chickenMaxX > carMinX && chickenMinX < carMaxX) {
-                //   endDOM.style.visibility = 'visible';
-                // }
+                if (chickenMaxX > carMinX && chickenMinX < carMaxX) {
+                    localIsDead = true;
+                    setIsDead(true);
+                    chicken.scale.z = 0.2;
+                    cameraSpeed = 0;
+                }
               });
             }
 
             if(lanes[currentLane].type === 'river') {
               const chickenMinX = chicken.position.x - chickenSize*zoom/2;
               const chickenMaxX = chicken.position.x + chickenSize*zoom/2;
-            //   console.log(lanes[currentLane].rafts, chicken)
               lanes[currentLane].rafts.forEach(raft => {
 
                 const raftMinX = raft.position.x - raft.geometry.parameters.width*zoom/2;
                 const raftMaxX = raft.position.x + raft.geometry.parameters.width*zoom/2;
                 if(chickenMaxX > raftMinX && chickenMinX < raftMaxX) {
-
                     chicken.position.x = raft.position.x
-
                     currentColumn = 17 - Math.round(( 1428 - (672 + chicken.position.x))/84)
                     camera.position.x = (chicken.position.x)
-                    // currentColumn = columns - Math.round(( boardWidth*zoom - (672 + chicken.position.x))/ positionWidth*zoom)
-                //   endDOM.style.visibility = 'visible';
-                // lanes[currentLane].direction
-                // ? (chicken.position.x -= lanes[currentLane].speed)
-                // : (chicken.position.x += lanes[currentLane].speed);
+                    dirLight.position.x = (chicken.position.x)
                 }
               });
             }
-            // console.log(lanes[currentLane])
-
             renderer.render( scene, camera );
           }
 
+          let timer = setInterval(() => {
+            if (currentLane > 0) {
+                camera.position.y += cameraSpeed;
+            }
+        }, 15);
 
-          let timer
-
-          if (chicken.position.y - 600 < camera.position.y + 100) {
-            console.log('!!!!!!!!')
-            timer = setInterval(() => {
-                camera.position.y += cameraSpeed
-
-              }, 15);
-          } else {
-              console.log('????????')
-            clearInterval(timer)
-          }
+        if (chicken.position.y - 600 > camera.position.y) {
+            clearInterval(timer);
+        }
 
           requestAnimationFrame( animate );
 
         renderer.render(scene, camera);
     }, []);
 
-    return <div ref={mountRef} />;
+    return (
+        <>
+            <div className={style.game} ref={mountRef} />
+            {isDead === true ? (
+                <div className={style.end}>
+                    <Restart restart={restart} setRestart={setRestart}/>
+                </div>
+            ) : null}
+        </>
+    );
 }
